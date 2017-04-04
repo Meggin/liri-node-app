5ee1bc72cbbbab7d4d1d714c8844ecb9e383812d
mkearney@google.com <mkearney@google.com>
2017-04-03 17:58:55 -0700

    first commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'oEYje63CqydfXeWDQEBZXJZx3',
+  consumer_secret: 'pPdVZC4fWEew4mUEwBUaVdk4LiJUoDITQPhK0DnFR7lSNcReYZ',
+  access_token_key: '3885326479-MDeI8XG9WXTrQQBiwAnnEPojjjbexRaFNa2Jydb',
+  access_token_secret: 'hWCOd0wnIWAMEux73ztg7kSzktHn3ecjgHN8q1QSBPLiE',
+}
---
+++ liri.js
@@ -0,0 +1,43 @@
+// Grab data from keys.js
+// Store keys in a variable.
+// Take in one of these commands:
+//
+// my-tweets
+// command: node liri.js my-tweets
+// Shows last 20 tweets and when they were created in terminal.
+//
+// spotify-this-song
+// command: node liri.js spotify-this-song '<song name here>'
+// Shows the following information about the song in terminal:
+// Artist(s)
+// song's name
+// preview link of song from Spotify,
+// album song is from.
+// If no song is provided, default to "The Sign" by Ace of Base.
+//
+// movie-this
+// command: node liri.js movie-this '<movie name here>'
+// Shows the following information about the movie in the terminal:
+// Title of the movie
+// Year the movie came out
+// IMDB raiging of the movie
+// Country where the movie was produced
+// Language of the movie
+// Plot of the movie
+// Actors in the movie
+// Rotten Tomatoes rating
+// Rotten Tomatoes URL
+// If the user doesn't type a movie in, output data for movie, 'Mr. Nobody'. 
+//
+// do-what-it-says
+// command: node liri.js do-what-it-says
+// Using the fs Node package, take the text inside random.txt,
+// and use it to call one of LIRI's commands.
+// It should run spotify-this-song for "I Want it That Way".
+// Change text in that document to test out feature for other commands.
+//
+// Bonus
+// In addition to logging data to terminal,
+// output data to .txt file called log.txt.
+// Make sure to append each command run to the log.txt file.
+// Do not overwrite your file each time you run a command.
