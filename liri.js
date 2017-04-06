2c681c97a29bed013038d99b0c4fe68eb0ffb17f
mkearney@google.com <mkearney@google.com>
2017-04-06 13:00:31 -0700

    decent version of spotify though refactor will be nice

--- liri.js
+++ liri.js
@@ -1,52 +1,159 @@
-
+// **Todo: write a decent intro this time!
 
 // Needed to read files. Not being used yet!
 var fs = require("fs");
 
-// Access twitter API through twitter module.
-var Twitter = require("twitter");
+var action = process.argv[2];
 
-// Access twitter keys in keys.js.
-var twitterKeysFile = require("./keys.js");
+// Creates empty string to hold song title.
+var songTitle = "";
 
-// Takes in my-tweets command.
-var my%2Dtweets = process.argv[2];
+switch (action) {
+	case "my-tweets": 
+	getMyTweets();
+	break;
 
-// Passes twitter keys into call to Twitter API.
-var client = new Twitter(twitterKeysFile.twitterKeys);
+	case "spotify-this-song":
+	
+	// First get song title argument.
+	songTitle = getSongTitle();
 
-// Search parameters includes my tweets up to last 20 tweets;
-var params = {q: '@MegginKearney', count: 20};
+	if (songTitle === "") {
+		lookupSpecificSong();
+	} else {
+		// Get song information from Spotify.
+		getSongInfo(songTitle);
+	}
+	
+	break;
+}
 
-// Shows last 20 tweets and when created in terminal.
-client.get('search/tweets', params, function(error, tweets, response) {
-  if (!error) {
+// Function to show my last 20 tweets.
+function getMyTweets() {
 
-  	// Check we can access tweets statuses array.
-  	console.log(tweets.statuses);
+	// Access twitter API through twitter module.
+	var Twitter = require("twitter");
 
-  	// Loop through tweets and print out tweet text and create date.
-  	for (var i = 0; i < tweets.statuses.length; i++) {
-  		var tweetText = tweets.statuses[i].text;
-  		console.log("Here's the tweet's text: " + tweetText);
-  		var tweetCreationDate = tweets.statuses[i].created_at;
-  		console.log("Here's the tweet's creation date: " + tweetCreationDate);
-  	}
-  } else {
-  	console.log(error);
-  }
-});
+	// Access twitter keys in keys.js.
+	var twitterKeysFile = require("./keys.js");
+	
+	// Passes twitter keys into call to Twitter API.
+	var client = new Twitter(twitterKeysFile.twitterKeys);
+
+	// Search parameters includes my tweets up to last 20 tweets;
+	var params = {q: '@MegginKearney', count: 20};
+
+	// Shows last 20 tweets and when created in terminal.
+	client.get('search/tweets', params, function(error, tweets, response) {
+	  if (!error) {
+
+	  	// Loop through tweets and print out tweet text and create date.
+	  	for (var i = 0; i < tweets.statuses.length; i++) {
+	  		var tweetText = tweets.statuses[i].text;
+	  		console.log("Here's the tweet's text: " + tweetText);
+	  		var tweetCreationDate = tweets.statuses[i].created_at;
+	  		console.log("Here's the tweet's creation date: " + tweetCreationDate);
+	  	}
+	  } else {
+	  	console.log(error);
+	  }
+	});
+}
 
 //
 // spotify-this-song
 // command: node liri.js spotify-this-song '<song name here>'
+
 // Shows the following information about the song in terminal:
 // Artist(s)
 // song's name
 // preview link of song from Spotify,
 // album song is from.
 // If no song is provided, default to "The Sign" by Ace of Base.
-//
+
+function getSongTitle() {
+	// Stores all the song title arguments in array.
+	var songTitleArgument = process.argv;
+
+	// Loops through words in node argument.
+	// To be able to pass song title as a parameter to call to Spotify API.
+	for (var i = 3; i < songTitleArgument.length; i++) {
+		songTitle += songTitleArgument[i];
+	}
+
+	return songTitle;
+}
+
+function lookupSpecificSong() {
+
+	// Access spotify API through spotify module.
+	// ** Will most likely move all of these out of functions!
+	var spotify = require("spotify");
+
+
+	// Calls spotify API to retrieve a specific track, The Sign, Ace of Base.
+	spotify.lookup({type: 'track', id: '3DYVWvPh3kGwPasp7yjahc'}, function(err, data) {
+		if (err) {
+			console.error(err);
+			return
+		}
+
+		console.log(data);
+
+		// Priting the artist, track name, preview url, and album name.
+		console.log("Artist: " + data.artists[0].name);
+		console.log("Song: " + data.name);
+		console.log("Spotify preview URL: " + data.preview_url);
+		console.log("Album name: " + data.album.name);
+	});
+}
+
+function getSongInfo(songTitle) {
+
+	// Access spotify API through spotify module.
+	var spotify = require("spotify");
+
+	console.log("Song title: " + songTitle);
+
+	// Calls spotify API to retrieve a track.
+	spotify.search({type: 'track', query: songTitle}, function(err, data) {
+		if (err) {
+			console.error(err);
+			return
+		}
+
+		// **Todo: limit seems to be set to 20 no matter what!
+		// ** I tried adding limit, as part of search parameters,
+		// ** But it didn't seem to change things.
+		// ** So instead of limit, I've forced a return of first item for track.
+		// ** Seems like this could be better.
+		// Also, might be nice to see more than one item for the track?
+		var artistsArray = data.tracks.items[0].album.artists;
+
+		// Need this array to be able to push artists names to it.
+		var artistsNames = [];
+
+		// Pushing artists for track to array.
+		for (var i = 0; i < artistsArray.length; i++) {
+			artistsNames.push(artistsArray[i].name);
+		}
+
+		// Converting artists array to string, and making it pretty.
+		var artists = artistsNames.join(", ");
+
+		// Priting the artist(s), track name, preview url, and album name.
+		console.log("Artist(s): " + artists);
+		console.log("Song: " + data.tracks.items[0].name)
+		console.log("Spotify preview URL: " + data.tracks.items[0].preview_url)
+		console.log("Album name: " + data.tracks.items[0].album.name);
+	});
+	
+}
+
+
+
+
+
 // movie-this
 // command: node liri.js movie-this '<movie name here>'
 // Shows the following information about the movie in the terminal:
