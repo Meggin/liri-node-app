5b3ceca191f0046bf3c9702c1bdb88567996a32d
mkearney@google.com <mkearney@google.com>
2017-04-07 13:37:16 -0700

    decent version of assignment with bonus and cleaned up code

--- liri.js
+++ liri.js
@@ -1,67 +1,116 @@
-// **Todo: write a decent intro this time!
+/* LIRI is a Language Interpretation and Recognition Interface.
+It takes in an action request and an optional argument,
+such as a movie-title, both using node's process.argv.
 
-// Creates empty string to hold song title.
-var songTitle = "";
+Action requests include retrieving tweets from Twitter API,
+song information from Spotify API, and movie information from OMDB.
+LIRI also calls out to a random text file that can include any action or argument.
 
-// Creates empty string to hold movie title.
-var movieTitle = "";
+LIRI includes logging support both to the console and an output file, log.txt.
+It uses the npm node module, 'simple-node-logger', for it's logging solution.
+*/ 
 
-// Creates empty string to hold movie title.
-var randomArgument = "";
+// Required NPM modules and files.
+// ____________________________________________________________________________________
 
-var argument = "";
+// NPM module used to access Twitter API.
+var Twitter = require("twitter");
 
+// Used to access twitter keys in local file, keys.js.
+var twitterKeysFile = require("./keys.js");
+
+// NPM module used to access Spotify API.
+var spotify = require("spotify");
+
+// NPM module used to access OMDB API.
+var request = require("request");
+
+// NPM module used to read the random.txt file.
+var fs = require("fs");
+
+// Output file for logs.
+var filename = './log.txt';
+
+// NPM module used for logging solution.
+var log = require('simple-node-logger').createSimpleFileLogger( filename );
+
+// All log information printed to log.txt.
+log.setLevel('all');
+
+// Controller and required parameters.
+// ____________________________________________________________________________________
+
+
+// Action requested.
 var action = process.argv[2];
 
+// Optional argument to request specific information.
+// Based on action type.
+var argument = "";
+
+// Controller function that determines what action is taken,
+// and specific data to complete that action.
 doSomething(action, argument);
 
+// Switch operation used to determin which action to take.
 function doSomething(action, argument) {
 
+	/* Controls optional third argument.
+	Defines specific data relating to the action.
+	For example, when requesting song information,
+	you can pass in a song title.
+	*/ 
 	argument = getThirdArgument();
 
 	switch (action) {
+		
+		// Gets list of tweets.
 		case "my-tweets": 
 		getMyTweets();
 		break;
 
+		// Gets song information.
 		case "spotify-this-song":
 		
-		// First get song title argument.
-		songTitle = argument;
+		// First gets song title argument.
+		var songTitle = argument;
 
+		// If no song title provided, defaults to specific song.
 		if (songTitle === "") {
 			lookupSpecificSong();
+
+		// Else looks up song based on song title.
 		} else {
 			// Get song information from Spotify.
 			getSongInfo(songTitle);
 		}
-		
 		break;
 
+		// Gets movie information.
 		case "movie-this":
 
-		// First get movie title argument.
-		// **Todo: I must refactor this to have one function
-		// That gets process.argv[3] for every action.
-		// Realizing now that do-what-it-says won't work!
-		movieTitle = argument;
+		// First gets movie title argument.
+		var movieTitle = argument;
 
+		// If no movie title provided, defaults to specific movie.
 		if (movieTitle === "") {
 			getMovieInfo("Mr. Nobody");
+
+		// Else looks up song based on movie title.
 		} else {
 			getMovieInfo(movieTitle);
 		}
-
 		break;
 
+		// Gets text inside file, and uses it to do something.
 		case "do-what-it-says": 
-		
-		randomArgument = argument;
-		doWhatItSays(randomArgument);
+		doWhatItSays();
 		break;
 	}
 }
 
+// Returns option third argument, for example,
+// when requesting song information, include a song title.
 function getThirdArgument() {
 
 	// Stores all possible arguments in array.
@@ -77,12 +126,6 @@ function getThirdArgument() {
 
 // Function to show my last 20 tweets.
 function getMyTweets() {
-
-	// Access twitter API through twitter module.
-	var Twitter = require("twitter");
-
-	// Access twitter keys in keys.js.
-	var twitterKeysFile = require("./keys.js");
 	
 	// Passes twitter keys into call to Twitter API.
 	var client = new Twitter(twitterKeysFile.twitterKeys);
@@ -90,218 +133,137 @@ function getMyTweets() {
 	// Search parameters includes my tweets up to last 20 tweets;
 	var params = {q: '@MegginKearney', count: 20};
 
-	// Shows last 20 tweets and when created in terminal.
+	// Shows up to last 20 tweets and when created in terminal.
 	client.get('search/tweets', params, function(error, tweets, response) {
 	  if (!error) {
 
-	  	// Loop through tweets and print out tweet text and create date.
+	  	// Loops through tweets and prints out tweet text and creation date.
 	  	for (var i = 0; i < tweets.statuses.length; i++) {
 	  		var tweetText = tweets.statuses[i].text;
-	  		console.log("Here's the tweet's text: " + tweetText);
+	  		logOutput("Tweet Text: " + tweetText);
 	  		var tweetCreationDate = tweets.statuses[i].created_at;
-	  		console.log("Here's the tweet's creation date: " + tweetCreationDate);
+	  		logOutput("Tweet Creation Date: " + tweetCreationDate);
 	  	}
 	  } else {
-	  	console.log(error);
+	  	logOutput(error);
 	  }
 	});
 }
 
-//
-// spotify-this-song
-// command: node liri.js spotify-this-song '<song name here>'
-
-// Shows the following information about the song in terminal:
-// Artist(s)
-// song's name
-// preview link of song from Spotify,
-// album song is from.
-// If no song is provided, default to "The Sign" by Ace of Base.
-
-function getSongTitle() {
-	// Stores all the song title arguments in array.
-	var songTitleArgument = process.argv;
-
-	// Loops through words in node argument.
-	// To be able to pass song title as a parameter to call to Spotify API.
-	for (var i = 3; i < songTitleArgument.length; i++) {
-		songTitle += songTitleArgument[i];
-	}
-
-	return songTitle;
-}
-
-function lookupSpecificSong() {
-
-	// Access spotify API through spotify module.
-	// ** Will most likely move all of these out of functions!
-	var spotify = require("spotify");
-
-
-	// Calls spotify API to retrieve a specific track, The Sign, Ace of Base.
-	spotify.lookup({type: 'track', id: '3DYVWvPh3kGwPasp7yjahc'}, function(err, data) {
-		if (err) {
-			console.error(err);
-			return
-		}
-
-		console.log(data);
-
-		// Priting the artist, track name, preview url, and album name.
-		console.log("Artist: " + data.artists[0].name);
-		console.log("Song: " + data.name);
-		console.log("Spotify preview URL: " + data.preview_url);
-		console.log("Album name: " + data.album.name);
-	});
-}
-
+// Calls spotify API to retrieve song information for song title.
 function getSongInfo(songTitle) {
-
-	// Access spotify API through spotify module.
-	var spotify = require("spotify");
-
-	console.log("Song title: " + songTitle);
 
 	// Calls spotify API to retrieve a track.
 	spotify.search({type: 'track', query: songTitle}, function(err, data) {
 		if (err) {
-			console.error(err);
+			logOutput.error(err);
 			return
 		}
 
-		// **Todo: limit seems to be set to 20 no matter what!
-		// ** I tried adding limit, as part of search parameters,
-		// ** But it didn't seem to change things.
-		// ** So instead of limit, I've forced a return of first item for track.
-		// ** Seems like this could be better.
-		// Also, might be nice to see more than one item for the track?
+		/* The spotify node module defaults to 20 no matter what.
+		Attempted to add a limit, which seems to do nothing.
+		Homework requirements suggest we should only return one song.
+		Used array properties to retrict songs returns.
+		There could very well be a better way to do this.
+		But it's as close to requirements I could get using spotify module.
+		*/
 		var artistsArray = data.tracks.items[0].album.artists;
 
-		// Need this array to be able to push artists names to it.
+		// Array to hold artist names, when more than one artist exists for a song.
 		var artistsNames = [];
 
-		// Pushing artists for track to array.
+		// Pushes artists for track to array.
 		for (var i = 0; i < artistsArray.length; i++) {
 			artistsNames.push(artistsArray[i].name);
 		}
 
-		// Converting artists array to string, and making it pretty.
+		// Converts artists array to string, and makes it pretty.
 		var artists = artistsNames.join(", ");
 
-		// Priting the artist(s), track name, preview url, and album name.
-		console.log("Artist(s): " + artists);
-		console.log("Song: " + data.tracks.items[0].name)
-		console.log("Spotify preview URL: " + data.tracks.items[0].preview_url)
-		console.log("Album name: " + data.tracks.items[0].album.name);
+		// Prints the artist(s), track name, preview url, and album name.
+		logOutput("Artist(s): " + artists);
+		logOutput("Song: " + data.tracks.items[0].name)
+		logOutput("Spotify Preview URL: " + data.tracks.items[0].preview_url)
+		logOutput("Album Name: " + data.tracks.items[0].album.name);
 	});
 	
 }
 
+// When no song title provided, defaults to specific song, The Sign.
+function lookupSpecificSong() {
 
-// movie-this
-// command: node liri.js movie-this '<movie name here>'
-// Shows the following information about the movie in the terminal:
-// Title of the movie
-// Year the movie came out
-// IMDB raiging of the movie
-// Country where the movie was produced
-// Language of the movie
-// Plot of the movie
-// Actors in the movie
-// Rotten Tomatoes rating
-// Rotten Tomatoes URL
-// If the user doesn't type a movie in, output data for movie, 'Mr. Nobody'. 
+	// Calls spotify API to retrieve a specific track, The Sign, Ace of Base.
+	spotify.lookup({type: 'track', id: '3DYVWvPh3kGwPasp7yjahc'}, function(err, data) {
+		if (err) {
+			logOutput.error(err);
+			return
+		}
 
-//function getMovieTitle() {
+		// Prints the artist, track name, preview url, and album name.
+		logOutput("Artist: " + data.artists[0].name);
+		logOutput("Song: " + data.name);
+		logOutput("Spotify Preview URL: " + data.preview_url);
+		logOutput("Album Name: " + data.album.name);
+	});
+}
 
-	// Stores all the song title arguments in array.
-//	var movieTitleArgument = process.argv;
-
-	// Loops through words in node argument.
-	// To be able to pass song title as a parameter to call to Spotify API.
-//	for (var i = 3; i < movieTitleArgument.length; i++) {
-//		movieTitle += movieTitleArgument[i];
-//	}
-
-//	return movieTitle;
-//}
-
+// Passes a query URL to OMDB to retrieve movie information for movie title.
+// If no movie title provided, defaults to the movie, Mr. Nobody.
 function getMovieInfo(movieTitle) {
-	console.log("We are getting the movie!" + movieTitle);
 
-	// Include the request npm package.
-	var request = require("request");
-
-	// Run a request to the OMDB API with the movie specified
+	// Runs a request to the OMDB API with the movie specified.
 	var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&tomatoes=true&r=json";
-	// This line is just to help us debug against the actual URL.
-	console.log(queryUrl);
 
 	request(queryUrl, function(error, response, body) {
-	  // If the request is successful
+	  // If the request is successful...
 	  if (!error && response.statusCode === 200) {
 	    
-	    // Parse the body of the site and recover movie info.
+	    // Parses the body of the site and recovers movie info.
 	    var movie = JSON.parse(body);
 
-	    console.log(movie);
+	    // Prints out movie info.
+	    logOutput("Movie Title: " + movie.Title);
+	    logOutput("Release Year: " + movie.Year);
+	    logOutput("IMDB Rating: " + movie.imdbRating);
+	    logOutput("Country Produced In: " + movie.Country);
+	    logOutput("Language: " + movie.Language);
+	    logOutput("Plot: " + movie.Plot);
+	    logOutput("Actors: " + movie.Actors);
 
-	    // Print out movie info.
-	    console.log("Movie Title: " + movie.Title);
-	    console.log("Release Year: " + movie.Year);
-	    console.log("IMDB Rating: " + movie.imdbRating);
-	    console.log("Country Produced In: " + movie.Country);
-	    console.log("Language: " + movie.Language);
-	    console.log("Plot: " + movie.Plot);
-	    console.log("Actors: " + movie.Actors);
-
-	    // Need to use value in array,
-	    // As rotten tomato rating value is always n/a.
-	    // Seems like a little bug in API?
-	    console.log("Rotten Tomatoes Rating: " + movie.Ratings[2].Value);
-	    console.log("Rotten Tomatoes URL: " + movie.tomatoURL);
+	    // Had to set to array value, as there seems to be a bug in API response,
+	    // that always returns N/A for movie.tomatoeRatings.
+	    logOutput("Rotten Tomatoes Rating: " + movie.Ratings[2].Value);
+	    logOutput("Rotten Tomatoes URL: " + movie.tomatoURL);
 	  }
 	});
 }
 
-// do-what-it-says
-// command: node liri.js do-what-it-says
-// Using the fs Node package, take the text inside random.txt,
-// and use it to call one of LIRI's commands.
-// It should run spotify-this-song for "I Want it That Way".
-// Change text in that document to test out feature for other commands.
+// Uses fs node package to take the text inside random.txt,
+// and do something with it.
 function doWhatItSays() {
-	// Needed to read files. Not being used yet!
-	var fs = require("fs");
 
 	fs.readFile("random.txt", "utf8", function(err, data) {
 		if (err) {
-			console.error(err);
+			logOutput.error(err);
 		} else {
 
+			// Creates array with data.
+			var randomArray = data.split(",");
 
-			randomArray = data.split(",");
-
-
-			console.log(randomArray);
-
+			// Sets action to first item in array.
 			action = randomArray[0];
 
-			console.log("Random Action: " + action);
-
+			// Sets optional third argument to second item in array.
 			argument = randomArray[1];
 
-			console.log("Random argument: " + argument);
-
+			// Calls main controller to do something based on action and argument.
 			doSomething(action, argument);
 		}
 	});
 }
 
-
-
-// Bonus
-// In addition to logging data to terminal,
-// output data to .txt file called log.txt.
-// Make sure to append each command run to the log.txt file.
-// Do not overwrite your file each time you run a command.
+// Logs data to the terminal and output to a text file.
+function logOutput(logText) {
+	log.info(logText);
+	console.log(logText);
+}
